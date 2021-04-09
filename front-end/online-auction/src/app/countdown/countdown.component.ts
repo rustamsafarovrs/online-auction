4f4707e70092aaead4c9656cad2f2619ee4b5ece
rustamsafarovrs <rs.programmer@hotmail.com>
2021-04-09 20:45:06 +0500

    fix

--- front-end/online-auction/src/app/countdown/countdown.component.ts
+++ front-end/online-auction/src/app/countdown/countdown.component.ts
@@ -11,27 +11,22 @@ export class CountdownComponent implements OnInit {
 
   @Input() endTime: string;
 
-  endsString;
+  endsString = '00:00:00';
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.init();
-
     this.startCountdown();
-  }
-
-  private init(): void {
-    const now = moment();
-    const diff = moment(this.endTime).diff(now);
-    this.endsString = moment(diff).format('HH:mm:ss');
   }
 
   private startCountdown(): void {
     interval(1000).subscribe(() => {
       const now = moment();
       const diff = moment(this.endTime).diff(now);
+      if (diff < 0) {
+        return;
+      }
       this.endsString = moment(diff).format('HH:mm:ss');
     });
   }
